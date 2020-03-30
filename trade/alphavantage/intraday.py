22fb6194ba1d94569b5f301aa04fa2f8419957e4
Minifranger <clement.franger@gmail.com>
2020-03-30 22:00:18 +0200

    first draw

---
+++ trade/alphavantage/intraday.py
@@ -0,0 +1,34 @@
+import os
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+client = boto3.resource('dynamodb')
+
+
+def get(event, context):
+    logger.info('event : {event}'.format(event=event))
+    path, = validate_params(path=event.get('pathParameters'))
+
+    id = path.get('id')
+    if not id:
+        return failure(code=400, body='You should provide a id to your path parameters')
+
+    params = {
+        'TableName': os.environ['REDDIT_TABLE'],
+        'Key': {'id': id}
+    }
+
+    logger.info('Getting item {id}'.format(id=id))
+
+    try:
+        result = client.Table(params.get('TableName')).get_item(**params)
+        if not result.get('Item'):
+            return success(status_code=204, body=json.dumps(result.get('Item'), cls=DecimalEncoder))
+    except Exception as e:
+        return failure(body=e)
+
+    logger.info('Retrieved item {id}'.format(id=id))
+    return success(body=json.dumps(result.get('Item'), cls=DecimalEncoder))
