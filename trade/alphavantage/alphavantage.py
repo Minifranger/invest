4be713a0d25dd94261dc5b9687aff643e822c1c4
Minifranger <clement.franger@gmail.com>
2020-03-30 22:46:31 +0200

    intraday call

--- trade/alphavantage/alphavantage.py
+++ trade/alphavantage/alphavantage.py
@@ -1,2 +1,9 @@
-import requests
+import os
+from alpha_vantage.timeseries import TimeSeries
 
+
+def alphavantage_ts():
+    return TimeSeries(key=os.environ('ALPHAVANTAGE_SECRET'))
+
+
+ALPHAVANTAGE_TS = alphavantage_ts()
--- trade/alphavantage/intraday.py
+++ trade/alphavantage/intraday.py
@@ -2,34 +2,30 @@ import os
 import json
 import logging
 import boto3
+from trade.alphavantage.alphavantage import ALPHAVANTAGE_TS
+from utils import validate_params, failure
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-client = boto3.resource('dynamodb')
 
 
 def intraday(event, context):
     logger.info('event : {event}'.format(event=event))
     path, = validate_params(path=event.get('pathParameters'))
 
-    id = path.get('id')
-    if not id:
-        return failure(code=400, body='You should provide a id to your path parameters')
+    symbol = path.get('symbol')
+    if not symbol:
+        return failure(code=400, body='You should provide a symbol to your path parameters')
 
-    params = {
-        'TableName': os.environ['REDDIT_TABLE'],
-        'Key': {'id': id}
-    }
-
-    logger.info('Getting item {id}'.format(id=id))
+    logger.info('Getting stock intraday {symbol}'.format(symbol=symbol))
 
     try:
-        result = client.Table(params.get('TableName')).get_item(**params)
-        if not result.get('Item'):
-            return success(status_code=204, body=json.dumps(result.get('Item'), cls=DecimalEncoder))
+        result = ALPHAVANTAGE_TS.get_intraday(**event.get('pathParameters'))
+        print(result)
+        # if not result.get('Item'):
+        #     return success(status_code=204, body=json.dumps(result.get('Item'), cls=DecimalEncoder))
     except Exception as e:
         return failure(body=e)
-
-    logger.info('Retrieved item {id}'.format(id=id))
-    return success(body=json.dumps(result.get('Item'), cls=DecimalEncoder))
+    #
+    # logger.info('Retrieved item {id}'.format(id=id))
+    # return success(body=json.dumps(result.get('Item'), cls=DecimalEncoder))
