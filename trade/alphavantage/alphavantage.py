4df531f32b552dd26b276bdcfb2ed5046f9c6ebf
Minifranger <clement.franger@gmail.com>
2020-06-20 22:38:41 +0200

    renamed project

--- trade/alphavantage/alphavantage.py
+++
@@ -1,9 +0,0 @@
-import os
-from alpha_vantage.timeseries import TimeSeries
-
-
-def alphavantage_ts():
-    return TimeSeries(key=os.environ('ALPHAVANTAGE_SECRET'))
-
-
-ALPHAVANTAGE_TS = alphavantage_ts()
